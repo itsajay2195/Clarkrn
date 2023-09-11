1d04d708f761729f0cf96864475752ec41a6e778
Ajaykumar Rajasekaran <ajaykumarrajasekaran@Ajaykumars-MacBook-Pro.local>
2023-09-11 20:50:12 +0530

    created a separated component for productListItem

--- src/screens/ProductListScreen/ProductListScreen.js
+++ src/screens/ProductListScreen/ProductListScreen.js
@@ -9,6 +9,7 @@ import {
 } from 'react-native';
 import React from 'react';
 import {DATA} from '../../data';
+import ProductListItem from './components/ProductListItem';
 
 const BG_HOME =
   'https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
@@ -21,87 +22,18 @@ const ProductListScreen = () => {
         style={StyleSheet.absoluteFillObject}
         blurRadius={80}
       />
+
       <Animated.FlatList
         data={DATA.products}
         onScroll={Animated.event(
           [{nativeEvent: {contentOffset: {y: scrollY}}}],
           {useNativeDriver: true},
         )}
-        contentContainerStyle={{
-          padding: 10,
-          paddingTop: StatusBar.currentHeight || 42,
-        }}
-        keyExtractor={item => item.id}
-        renderItem={({item, index}) => {
-          const inputRange = [-1, 0, 90 * index, 90 * (index + 2)];
-          const opacityInputRange = [-1, 0, 90 * index, 90 * (index + 1.25)];
-          const scale = scrollY.interpolate({
-            inputRange,
-            outputRange: [1, 1, 1, 0],
-          });
-          const opacity = scrollY.interpolate({
-            inputRange: opacityInputRange,
-            outputRange: [1, 1, 1, 0],
-          });
-          return (
-            <Animated.View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                height: 80,
-                backgroundColor: 'rgba(255,255,255,0.8)',
-                padding: 10,
-                marginBottom: 10,
-                borderRadius: 14,
-                shadowColor: '%000',
-                shadowOffset: {
-                  width: 0,
-                  height: 10,
-                },
-                opacity,
-                shadowOpacity: 0.5,
-                shadowRadius: 20,
-                transform: [{scale}],
-              }}>
-              <TouchableOpacity style={{flex: 1, flexDirection: 'row'}}>
-                <View style={{flex: 0.35, justifyContent: 'center'}}>
-                  <Image
-                    source={{uri: item.thumbnail}}
-                    style={{height: 60, width: 60, borderRadius: 60}}
-                    resizeMode={'contain'}
-                  />
-                </View>
-                <View style={{flex: 1, justifyContent: 'center'}}>
-                  <Text
-                    style={{fontSize: 20, fontWeight: '700', color: 'black'}}
-                    numberOfLines={1}>
-                    {item.title}
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      fontWeight: '700',
-                      opacity: 0.8,
-                      color: 'grey',
-                    }}
-                    numberOfLines={1}>
-                    {item.brand}
-                  </Text>
-                </View>
-                <View
-                  style={{
-                    flex: 0.4,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}>
-                  <Text style={{fontWeight: '700', fontSize: 16}}>
-                    {item.price}
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            </Animated.View>
-          );
-        }}
+        contentContainerStyle={styles.contentContainerStyle}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item, index}) => (
+          <ProductListItem item={item} index={index} scrollY={scrollY} />
+        )}
       />
     </View>
   );
@@ -109,4 +41,9 @@ const ProductListScreen = () => {
 
 export default ProductListScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  contentContainerStyle: {
+    padding: 10,
+    paddingTop: StatusBar.currentHeight || 42,
+  },
+});
---
+++ src/screens/ProductListScreen/components/ProductListItem.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Animated,
+  StyleSheet,
+} from 'react-native';
+
+interface Product {
+  id: string;
+  title: string;
+  brand: string;
+  thumbnail: string;
+  price: string;
+}
+
+interface ProductListItemProps {
+  item: Product;
+  index: number;
+  scrollY: Animated.Value;
+}
+
+const ProductListItem: React.FC<ProductListItemProps> = ({
+  item,
+  index,
+  scrollY,
+}) => {
+  const inputRange = [-1, 0, 90 * index, 90 * (index + 2)];
+  const opacityInputRange = [-1, 0, 90 * index, 90 * (index + 1.25)];
+
+  const scale = scrollY.interpolate({
+    inputRange,
+    outputRange: [1, 1, 1, 0],
+  });
+
+  const opacity = scrollY.interpolate({
+    inputRange: opacityInputRange,
+    outputRange: [1, 1, 1, 0],
+  });
+
+  return (
+    <Animated.View
+      style={[
+        styles.productItem,
+        {
+          opacity,
+          transform: [{scale}],
+        },
+      ]}>
+      <TouchableOpacity style={styles.itemContainer}>
+        <View style={styles.thumbnailContainer}>
+          <Image
+            source={{uri: item.thumbnail}}
+            style={styles.thumbnail}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.title} numberOfLines={1}>
+            {item.title}
+          </Text>
+          <Text style={styles.brand} numberOfLines={1}>
+            {item.brand}
+          </Text>
+        </View>
+        <View style={styles.priceContainer}>
+          <Text style={styles.price}>₹ {item.price}</Text>
+        </View>
+      </TouchableOpacity>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  productItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 80,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 14,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 20,
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  thumbnailContainer: {
+    flex: 0.35,
+    justifyContent: 'center',
+  },
+  thumbnail: {
+    height: 60,
+    width: 60,
+    borderRadius: 60,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: 'black',
+    marginRight: 10,
+  },
+  brand: {
+    fontSize: 16,
+    fontWeight: '700',
+    opacity: 0.8,
+    color: 'grey',
+  },
+  priceContainer: {
+    flex: 0.3,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  price: {
+    fontWeight: '700',
+    fontSize: 16,
+  },
+});
+
+export default ProductListItem;
